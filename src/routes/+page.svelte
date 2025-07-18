93a94c15ebc4dd180da558644407b722b5d821bd
Adrian <adunham95@gmail.com>
2025-07-18 00:17:29 -0400

    Removed form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -429,37 +429,3 @@
 		</div>
 	</div>
 </div>
-
-<!-- Email Sign UP -->
-<!-- <div id="signup" class=" py-16 sm:py-24 lg:py-32">
-	<div class="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
-		<h2
-			class="max-w-xl text-3xl font-semibold tracking-tight text-balance text-gray-900 sm:text-3xl lg:col-span-7"
-		>
-			Want to be the first to try it?<br /> Sign up for our wait list
-		</h2>
-		<form class="w-full max-w-md lg:col-span-5 lg:pt-2" method="POST" action="?/emailSignUp">
-			<div class="flex gap-x-4">
-				<label for="email-address" class="sr-only">Email address</label>
-				<input
-					id="email-address"
-					name="email"
-					type="email"
-					autocomplete="email"
-					required
-					class="focus:outline-primary min-w-0 flex-auto rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-					placeholder="Enter your email"
-				/>
-				<div class="cf-turnstile" data-sitekey={PUBLIC_CLOUDFLARE_TURNSTYLE_SITE_KEY}></div>
-				<button type="submit" class="btn btn--primary">Sign up</button>
-			</div>
-			<p class="mt-4 text-sm/6 text-gray-900">
-				We care about your data. Read our <a
-					href="/privacy-policy"
-					class="text-primary hover:text-primary-400 font-semibold whitespace-nowrap"
-					>privacy policy</a
-				>.
-			</p>
-		</form>
-	</div>
-</div> -->
