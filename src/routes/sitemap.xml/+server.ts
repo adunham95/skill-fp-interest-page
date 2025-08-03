d4ab8dc50684543f97d15e3e11e27f6cee6c1c0d
Adrian <adunham95@gmail.com>
2025-08-03 19:39:01 -0400

    Updated sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,44 @@
+// import { createClient } from '@prismicio/client';
+import { createClient } from '$lib/prismicio';
+
+export const GET = async ({ fetch, cookies }) => {
+	const client = createClient({ fetch, cookies });
+	const documents = await client.getAllByType('page');
+	const homePage = await client.getAllByType('homepage');
+
+	console.log({ documents, homePage });
+
+	const homepageData = homePage.map((doc) => {
+		return `<url>
+        <loc>https://career-fingerprint.com/</loc>
+        <lastmod>${doc.last_publication_date}</lastmod>
+        <priority>1.00</priority>
+    </url> `;
+	});
+
+	const urls = documents.map((doc) => {
+		return `
+	  <url>
+	    <loc>https://career-fingerprint.com${doc.url}</loc>
+	    <lastmod>${doc.last_publication_date}</lastmod>
+        <priority>0.8</priority>
+	  </url>`;
+	});
+
+	const xml = `<?xml version="1.0" encoding="UTF-8" ?>
+	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${homepageData}
+        <url>
+            <loc>https://career-fingerprint.com/use-cases</loc>
+            <lastmod>2025-07-01T16:55:01+00:00</lastmod>
+            <priority>0.80</priority>
+        </url>
+	  ${urls.join('\n')}
+	</urlset>`;
+
+	return new Response(xml, {
+		headers: {
+			'Content-Type': 'application/xml'
+		}
+	});
+};
