19174f342005197e1c0d462bb53d3f2759d68344
Adrian <adunham95@gmail.com>
2025-08-06 21:51:36 -0400

    Added tracking

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,6 @@
 // export const prerender = true;
+
+import { PUBLIC_MIXPANEL_TOKEN } from '$env/static/public';
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init(PUBLIC_MIXPANEL_TOKEN, { debug: false, track_pageview: true });
