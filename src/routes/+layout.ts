c95c4db6ee006637497bd6eb885fb5ca9bb6bed4
Adrian <adunham95@gmail.com>
2025-08-15 17:46:06 -0400

    Updated with heatmap tracking

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,4 +3,15 @@
 import { PUBLIC_MIXPANEL_TOKEN } from '$env/static/public';
 import mixpanel from 'mixpanel-browser';
 
-mixpanel.init(PUBLIC_MIXPANEL_TOKEN, { debug: false, track_pageview: true });
+mixpanel.init(PUBLIC_MIXPANEL_TOKEN, {
+	debug: false,
+	track_pageview: true,
+	autocapture: {
+		pageview: 'full-url',
+		click: true, // click tracking enabled
+		scroll: true,
+		submit: true,
+		capture_text_content: false
+	},
+	record_sessions_percent: 1
+});
