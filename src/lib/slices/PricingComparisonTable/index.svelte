112f50ea72075d3a89d7f45f49005073222c8750
Adrian <adunham95@gmail.com>
2025-09-28 06:31:15 -0400

    Added accent color

--- src/lib/slices/PricingComparisonTable/index.svelte
+++ src/lib/slices/PricingComparisonTable/index.svelte
@@ -84,7 +84,7 @@
 										fill="currentColor"
 										data-slot="icon"
 										aria-hidden="true"
-										class="text-primary h-6 w-5 flex-none"
+										class="text-accent h-6 w-5 flex-none"
 									>
 										<path
 											d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
@@ -187,7 +187,7 @@
 											fill="currentColor"
 											data-slot="icon"
 											aria-hidden="true"
-											class="text-primary mx-auto size-5"
+											class="text-accent mx-auto size-5"
 										>
 											<path
 												d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
@@ -222,7 +222,7 @@
 											fill="currentColor"
 											data-slot="icon"
 											aria-hidden="true"
-											class="text-primary mx-auto size-5"
+											class="text-accent mx-auto size-5"
 										>
 											<path
 												d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
