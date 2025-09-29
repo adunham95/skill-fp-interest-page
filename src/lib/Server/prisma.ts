1f1776e2650d0505737d09e6b291644e3d182e0e
Adrian <adunham95@gmail.com>
2025-09-29 04:42:20 -0400

    Changed to use prisma

--- src/lib/Server/prisma.ts
+++ src/lib/Server/prisma.ts
@@ -1,18 +1,18 @@
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// const globalForPrisma = global as unknown as { prisma: typeof prisma };
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-// export { prisma };
-
-import { PrismaClient } from '../../../prisma/src/generated/prisma/client';
-import { POSTGRES_PRISMA_URL } from '$env/static/private';
-
-const prisma = new PrismaClient({
-	datasourceUrl: POSTGRES_PRISMA_URL
-});
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
+
+// import { PrismaClient } from '../../../prisma/src/generated/prisma/client';
+// import { DATABASE_URL } from '$env/static/private';
+
+// const prisma = new PrismaClient({
+// 	datasourceUrl: DATABASE_URL
+// });
+
+// export default prisma;
