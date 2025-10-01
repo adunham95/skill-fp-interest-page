e037f9a2f351c57d0daba490abcf1be4243134fe
Adrian <adunham95@gmail.com>
2025-09-30 21:17:31 -0400

    Removed curtain reveal animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,26 +6,13 @@
 	import Banner from '$lib/Components/Banner.svelte';
 	import Header from '$lib/Components/Header.svelte';
 	import Footer from '$lib/Components/Footer.svelte';
-	import CurtainReveal from '$lib/Components/CurtainReveal.svelte';
-	import { browser } from '$app/environment';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import ScrollIndicator from '$lib/Components/ScrollIndicator.svelte';
 
 	injectSpeedInsights();
 
 	let { children, data } = $props();
-	const showCurtain = $derived(() => {
-		if (!browser) {
-			return;
-		}
-		const urlParams = new URLSearchParams(window && window.location.search);
-		return urlParams.get('launch') === 'true';
-	});
 </script>
-
-{#if showCurtain()}
-	<CurtainReveal />
-{/if}
 
 {#if data.banner}
 	<Banner banner={data.banner} />
