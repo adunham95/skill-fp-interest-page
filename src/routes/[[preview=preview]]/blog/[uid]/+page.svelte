3b3d7f738995cc66a10102ce61349331e769b04a
Adrian <adunham95@gmail.com>
2025-10-16 18:55:17 -0400

    Updated og image url

--- src/routes/[[preview=preview]]/blog/[uid]/+page.svelte
+++ src/routes/[[preview=preview]]/blog/[uid]/+page.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <Seo
-	meta_image_url={`https://mycareerginfingerprint.com/api/og?message=${data.page.data.meta_title?.toString()}`}
+	meta_image_url={`https://mycareerfingerprint.com/api/og?message=${data.page.data.meta_title?.toString()}`}
 	pageName={data.page.data.meta_title?.toString()}
 	meta_description={data.page.data.meta_description?.toString()}
 />
