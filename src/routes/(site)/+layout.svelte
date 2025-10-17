0d18654bd96d667a8c26b15688bbb7c23171ad49
Adrian <adunham95@gmail.com>
2025-10-17 15:17:40 -0400

    Added public clarity

--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -66,6 +66,16 @@
 		`}
 	{/if}
 
+	{#if PUBLIC_CLARITY_ID}
+		{@html `<script>
+        (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", ${PUBLIC_CLARITY_ID});
+        </script>`}
+	{/if}
+
 	<script>
 		var timeout = 3000; // Timeout value to remove the flicker (in milliseconds)
 		!(function (h, i, d, e) {
